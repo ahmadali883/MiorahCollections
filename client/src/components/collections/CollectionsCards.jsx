0dc3c6791f9032a44674bc2d5a20b3e3fcb0f3b5
ahmadali883 <khahmadalikhan@gmail.com>
2025-05-28 16:54:06 +0500

    Categories added

--- client/src/components/collections/CollectionsCards.jsx
+++ client/src/components/collections/CollectionsCards.jsx
@@ -1,86 +1,53 @@
-import Nike from '../../assets/collections/nike-logo.svg';
-import Adidas from '../../assets/collections/adidas-logo.svg';
-import Puma from '../../assets/collections/puma-logo.svg';
-import Reebok from '../../assets/collections/reebok-logo.svg';
-import Vans from '../../assets/collections/vans-logo.svg';
-import Jordan from '../../assets/collections/jordan-logo.svg';
 import { Link } from 'react-router-dom';
-import { getProductsByCollection } from '../../redux/reducers/productSlice';
 import { useDispatch } from 'react-redux';
+import { getProductsByCollection } from '../../redux/reducers/productSlice';
+
+// Dummy images for categories
+const categoryImages = {
+  bracelet: 'https://images.unsplash.com/photo-1611652022419-a9419f74343d?w=500&auto=format&fit=crop&q=60',
+  necklace: 'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=500&auto=format&fit=crop&q=60',
+  rings: 'https://images.unsplash.com/photo-1605100804763-247f67b3557e?w=500&auto=format&fit=crop&q=60',
+  earrings: 'https://images.unsplash.com/photo-1630019852942-f89202989a59?w=500&auto=format&fit=crop&q=60',
+  pendants: 'https://images.unsplash.com/photo-1611652022419-a9419f74343d?w=500&auto=format&fit=crop&q=60',
+  handcuffs: 'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=500&auto=format&fit=crop&q=60',
+  'bridal-sets': 'https://images.unsplash.com/photo-1605100804763-247f67b3557e?w=500&auto=format&fit=crop&q=60'
+};
 
 const CollectionsCards = () => {
   const dispatch = useDispatch();
 
+  const categories = [
+    { name: 'Bracelet', id: 'bracelet' },
+    { name: 'Necklace', id: 'necklace' },
+    { name: 'Rings', id: 'rings' },
+    { name: 'Earrings', id: 'earrings' },
+    { name: 'Pendants', id: 'pendants' },
+    { name: 'Handcuffs', id: 'handcuffs' },
+    { name: 'Bridal Sets', id: 'bridal-sets' }
+  ];
+
   return (
     <div className='collections-wrapper flex flex-wrap mt-12 justify-center mb-3 lg:mb-12'>
-      <Link to={`/collections/?collection=${'nike'}`} onClick={()=>dispatch(getProductsByCollection('nike'))}>
-      <div
-        before='Nike'
-        className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center jusitfy-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
-      >
-        <picture className='mx-auto'>
-          <img src={Nike} alt='nike logo' className='p-4 xl:p-0' />
-        </picture>
-      </div>
-      </Link>
-      {/* <Link to={`/products/${'adidas'}`}> */}
-      <Link to={`/collections/?collection=${'adidas'}`} onClick={()=>dispatch(getProductsByCollection('adidas'))}>
-
-      <div
-        before='Adidas'
-        className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center jusitfy-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
-      >
-        <picture className='mx-auto'>
-          <img src={Adidas} alt='adidas logo' className='p-4 xl:p-0' />
-        </picture>
-      </div>
-      </Link>
-      {/* <Link to={`/products/${'puma'}`}> */}
-      <Link to={`/collections/?collection=${'puma'}`} onClick={()=>dispatch(getProductsByCollection('puma'))}>
-
-      <div
-        before='Puma'
-        className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center jusitfy-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
-      >
-        <picture className='mx-auto'>
-          <img src={Puma} alt='puma logo' className='p-4 xl:p-0' />
-        </picture>
-      </div>
-      </Link>
-      {/* <Link to={`/products/${'reebok'}`}>   */}
-      <Link to={`/collections/?collection=${'reebok'}`} onClick={()=>dispatch(getProductsByCollection('reebok'))}>
-
-      <div
-        before='Reebok'
-        className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center jusitfy-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
-      >
-        <picture className='mx-auto'>
-          <img src={Reebok} alt='reebok logo' className='p-4 xl:p-0' />
-        </picture>
-      </div>
-      </Link>
-      {/* <Link to={`/products/${'vans'}`}> */}
-      <Link to={`/collections/?collection=${'vans'}`} onClick={()=>dispatch(getProductsByCollection('vans'))}>
-      <div
-        before='Vans'
-        className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center jusitfy-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
-      >
-        <picture className='mx-auto'>
-          <img src={Vans} alt='vans logo' className='p-4 xl:p-0' />
-        </picture>
-      </div>
-      </Link>
-      {/* <Link to={`/products/${'jordan'}`}> */}
-      <Link to={`/collections/?collection=${'jordan'}`} onClick={()=>dispatch(getProductsByCollection('jordan'))}>
-      <div
-        before='Jordan'
-        className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center jusitfy-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
-      >
-        <picture className='mx-auto'>
-          <img src={Jordan} alt='jordan logo' className='p-4 xl:p-0' />
-        </picture>
-      </div>
-    </Link>
+      {categories.map((category) => (
+        <Link 
+          key={category.id}
+          to={`/collections/?collection=${category.id}`} 
+          onClick={() => dispatch(getProductsByCollection(category.id))}
+        >
+          <div
+            before={category.name}
+            className='w-[15rem] bg-pale-orange xl:w-[19.5rem] text-pale-orange text-xl md:text-2xl uppercase cursor-pointer h-48 xl:h-60 border border-grayish-blue rounded-md mx-4 mb-10 flex items-center justify-center relative after:content-[attr(before)] after:absolute after:flex after:w-full after:justify-center after:opacity-0 hover:after:opacity-100 before:absolute before:bg-[rgba(255,_125,_27,_0.9)] before:inset-0 text-center before:h-0 hover:before:h-full before:transition-all'
+          >
+            <picture className='mx-auto'>
+              <img 
+                src={categoryImages[category.id]} 
+                alt={`${category.name} collection`} 
+                className='p-4 xl:p-0 w-full h-full object-cover'
+              />
+            </picture>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
